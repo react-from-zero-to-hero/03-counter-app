220014902f6102d507b639619ef58ff0b65c752c
Alberth <alberthbernarth@gmail.com>
2022-03-02 17:45:33 -0400

    starting homework

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PrimeraApp from './PrimeraApp'
+import PrimeraApp from './components/PrimeraApp'
 import './index.css';
 
 const divRoot = document.querySelector('#root');
