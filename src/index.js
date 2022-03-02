c628203ceb234d2feab4b809d22e8910169a5a06
Alberth <alberthbernarth@gmail.com>
2022-03-02 17:52:36 -0400

    attempt to create a CounterApp component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PrimeraApp from './components/PrimeraApp'
+import CounterApp from './components/CounterApp';
 import './index.css';
 
 const divRoot = document.querySelector('#root');
 
 ReactDOM.render( 
     <React.StrictMode>
-        <PrimeraApp saludo='Hola Mundo'/>
+        <CounterApp value={ 0 } />
     </React.StrictMode>,
     divRoot 
 );
