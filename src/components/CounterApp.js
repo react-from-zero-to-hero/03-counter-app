3584f8bf7b054422acecadf64bd62567d73923b2
Alberth <alberthbernarth@gmail.com>
2022-03-02 21:54:17 -0400

    counter component

--- src/components/CounterApp.js
+++ src/components/CounterApp.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const CounterApp = ({ value }) => {
+
+    const [ counter, setCounter ] = useState( value );
+
+    // handleAdd
+    const handleAdd = () => {
+        setCounter( counter + 1 );
+    }
+
+    const handleSubstract = () => {
+        setCounter( counter - 1 );
+    }
+
+    const handleReset = () => {
+        setCounter( value );
+    }
+
     return(
         <>
             <h1>CounterApp</h1>
-            <h2> { value } </h2>
+            <h2> { counter } </h2>
+            {/* Pasamos como referencia la función por eso no lo llamamos como handleAdd() */}
+            <button onClick={ handleAdd }>+1</button>
+            <button onClick={ handleReset }>Reset</button>
+            <button onClick={ handleSubstract }>-1</button>
         </>
     );
 }
